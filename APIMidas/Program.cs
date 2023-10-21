b6e1d19596295e92944b0e31062b0921b13f3177
freshhuk <freshhuk13@gmail.com>
2023-10-21 15:12:30 +0300

    Start write unit tests, and created cors

--- APIMidas/Program.cs
+++ APIMidas/Program.cs
@@ -1,3 +1,8 @@
+using DomainMidas.Entities;
+using InfrastructureMidas.Context;
+using InfrastructureMidas.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
@@ -5,6 +10,34 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//HttpClient Cors
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin", builder =>
+    {
+        builder.WithOrigins("http://localhost:5157/") //ссылка на UI хост
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
+
+// Configure app configuration
+builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables();
+
+
+// Create DbContext with configuration
+builder.Services.AddDbContext<IDataContext<Product>, FavouriteProductContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+builder.Services.AddScoped<FavouriteProductContext>(provider =>
+{
+    var configuration = provider.GetRequiredService<IConfiguration>();
+    return new FavouriteProductContext(configuration);
+});
 
 var app = builder.Build();
 
@@ -16,6 +49,12 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseCors("AllowSpecificOrigin");//HttpClient
+
+
 
 app.UseAuthorization();
 
